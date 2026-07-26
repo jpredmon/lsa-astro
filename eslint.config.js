ac1591dfaf84da432e152ea2fe22cbf78644a9ce
JP Redmon <jpredmon@outlook.com>
2026-07-26 16:38:52 -0500

    chore: port lint/format tooling from legacy project

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default defineConfig(
+  globalIgnores(['dist', '.astro']),
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  eslintPluginAstro.configs['jsx-a11y-recommended'],
+  {
+    files: ['**/*.{ts,astro}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    },
+  }
+);
